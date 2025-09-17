e68d53a2c4572cf2249ba463fb602d16714cbd15
Oleksandr Potomkin <77549025+dext2029@users.noreply.github.com>
2025-09-17 20:53:01 +0000

    drobne malickosti

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,20 +33,7 @@
     }
   };
 
-  //===== close navbar-collapse when a  clicked
-  let navbarToggler = document.querySelector(".navbar-toggler");
-  const navbarCollapse = document.querySelector(".navbar-collapse");
-
-  document.querySelectorAll(".ud-menu-scroll").forEach((e) =>
-    e.addEventListener("click", () => {
-      navbarToggler.classList.remove("active");
-      navbarCollapse.classList.remove("show");
-    })
-  );
-  navbarToggler.addEventListener("click", function () {
-    navbarToggler.classList.toggle("active");
-    navbarCollapse.classList.toggle("show");
-  });
+  
 
   // ===== submenu
   const submenuButton = document.querySelectorAll(".nav-item-has-children");
